4909d6773bf0c2f926a40f6284db5fc743cf6b24
lalicon05 <lasse.l535@gmail.com>
2025-10-16 10:37:41 +0200

    added the complete snowflake function

--- .venv/snowflake.py
+++ .venv/snowflake.py
@@ -22,7 +22,23 @@ def snowflake(turtle, depth):
         snowflake(turtle, depth-1)  #                    F
 
 
+"""
+Our own complete snowflake function
+Enter the amount of times it should complete the "snowflake" function to have more controll
+"""
+
+def complete_snowflake(turtle, depth, sides=3):
+    """
+    turtle : turtle object "Turtle(interactive=False)"
+    depth : int, how deep the snowflake should go
+    sides : int, how many times it should make turns and draw the snowflake function default = 3
+    """
+    turn = 360 / sides
+    for i in range(sides):
+        snowflake(turtle, depth)
+        turtle.right(turn)
+
 t = Turtle(interactive=False)
 
-snowflake(t, 4)
+complete_snowflake(t, 2, sides=3)
 plt.show()
