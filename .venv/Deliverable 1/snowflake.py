690084c0c8e8f964eb22384c6f2ec54bdbc836bf
lalicon05 <lasse.l535@gmail.com>
2025-10-16 09:58:40 +0200

    ran the first code

--- .venv/Deliverable 1/snowflake.py
+++
@@ -1,6 +0,0 @@
-import turtleplotlib as turtle
-
-
-
-if __name__ == "__main__":
-    print("hello world!")
---
+++ .venv/snowflake.py
@@ -0,0 +1,10 @@
+from turtleplotlib import Turtle
+
+t = Turtle(interactive=False)
+
+t.forward(100)
+t.left(90)
+t.color("red")
+t.forward(50)
+
+print("done")
