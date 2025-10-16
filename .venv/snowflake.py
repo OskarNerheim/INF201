8f99a79dd8f0c2037498d3603a6e1c6113176777
lalicon05 <lasse.l535@gmail.com>
2025-10-16 10:23:22 +0200

    copied the snowflake from the notebook and added a comment refering to its origin.

--- .venv/snowflake.py
+++ .venv/snowflake.py
@@ -1,13 +1,28 @@
 from turtleplotlib import Turtle
 import matplotlib.pyplot as plt
 
+
+
+"""
+Snowflake function gotten from the course notebook: INF201_H25_D1_Intro.ipynb
+"""
+
+
+def snowflake(turtle, depth):
+    if depth == 0:
+        turtle.forward(10)          # utfør F
+    else:                          # F ->
+        snowflake(turtle, depth-1)  #      F
+        turtle.left(60)             #        +
+        snowflake(turtle, depth-1)  #          F
+        turtle.right(60)            #            -
+        turtle.right(60)            #              -  
+        snowflake(turtle, depth-1)  #                F
+        turtle.left(60)             #                  +
+        snowflake(turtle, depth-1)  #                    F
+
+
 t = Turtle(interactive=False)
 
-t.forward(100)
-t.left(90)
-t.color("red")
-t.forward(50)
-
-print("done")
-
-plt.show()  # <-- this displays the drawing window
+snowflake(t, 4)
+plt.show()
