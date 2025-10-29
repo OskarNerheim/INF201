450e8d913365b033bd7b4e64676a6bc67e086055
lalicon05 <lasse.l535@gmail.com>
2025-10-29 16:30:30 +0100

    explain where the original rect class is from, add the draw function and run it in main.

--- Deliverable 2/drawing.py
+++ Deliverable 2/drawing.py
@@ -1,27 +1,48 @@
+from turtleplotlib import Turtle
+import matplotlib.pyplot as plt
+
 
 class Rectangle:
+    """
+    Original Rectangle class fetched from jupyter notebook for week 8 in the INF201 canvas room.
+    """
     def __init__(self, lower_left, upper_right):
         assert isinstance(lower_left, tuple), "lower_left must be a tuple"
         assert isinstance(upper_right, tuple), "upper_right must be a tuple"
         assert len(lower_left) == 2, "lower_left must contain exactly 2 elements"
         assert len(upper_right) == 2, "upper_right must contain exactly 2 elements"
         
-        self._ll = lower_left
-        self._width = upper_right[0] - lower_left[0]
-        self._height = upper_right[1] - lower_left[1]
-        self._lower_left = lower_left
-        self._upper_right = upper_right
+        self.lower_left = lower_left
+        self.upper_right = upper_right
+        self.width = upper_right[0] - lower_left[0]
+        self.height = upper_right[1] - lower_left[1]
 
     def info(self):
-        print(f"Upper right: {self._upper_right}, Lower Left: {self._lower_left}")
+        print(f"Upper right: {self.upper_right}, Lower Left: {self.lower_left}")
         
     def area(self):
-        return self._width * self._height
+        return self.width * self.height
+
+    def draw(self):
+        #move to lower left corner and draw the rectangle
+        t.up()
+        t.x, t.y = self.lower_left
+        t.down()
+        for i in range(2):
+            t.forward(self.width)
+            t.left(90)
+            t.forward(self.height)
+            t.left(90)
 
 
 
 if __name__ == "__main__":
-    shapes = [Rectangle((1,1), (2,2)), Rectangle((0,0), (2,3))]
-    for shape in shapes:
-        print(shape.area())
+    
+    t = Turtle(interactive=False)
+
+    shapes = [Rectangle((10, 10), (20,20)), Rectangle((10,20), (20,30)), Rectangle((20, 40), (30, 50))]
+    for shape in shapes:    
         shape.info()
+        shape.draw()
+    
+    plt.show()
