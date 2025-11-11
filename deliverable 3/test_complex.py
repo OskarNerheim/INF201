68e254a5559eb3179a031b57c38d7c9a86911884
lalicon05 <lasse.l535@gmail.com>
2025-11-11 16:58:39 +0100

    added tests

--- deliverable 3/test_complex.py
+++ deliverable 3/test_complex.py
@@ -1,7 +1,20 @@
 from complex import Complex
 
 z = Complex(1, 3)
-y = Complex(1, 3)
+y = Complex(2, 4)
+
+
+#Expected answers
+print("Expected answers:")
+print("Complex number a (1 + 3i)")
+print("complex number b (2 + 4i)")
+print("addition: a + b = 3 + 7i")
+print("subtraction: a - b = ")
+
+print("Sub")
+print(" a - b", " 2 + 2i", "true / false")
+
+
 
 print(z)
 print([z, y])
