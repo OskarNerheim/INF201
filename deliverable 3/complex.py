f6bf73fc376620ec7402deba0a80d8a1f396e43b
lalicon05 <lasse.l535@gmail.com>
2025-11-11 14:21:06 +0100

    fixed add so the 3 + complex case works

--- deliverable 3/complex.py
+++ deliverable 3/complex.py
@@ -5,25 +5,29 @@ class Complex:
         self.im = b
 
     def __str__(self):
-        return f"{self.re} + {self.im}i"
+        return f"{self.re}+{self.im}i"
     
     def __repr__(self):
         return f"Complex({self.re}, {self.im})"
 
     def __add__(self, other):
-        new_re = self.re + other.re
-        new_im = self.im + other.im
-        return f"{new_re} + {new_im}i"
+        if isinstance(other, (int, float)):
+            return Complex(self.re + other, self.im)
+        elif isinstance(other, Complex):
+            return Complex(self.re + other.re, self.im + other.im)
+    
+    def __radd__(self, other):
+        self.__add__(other)
 
     def __sub__(self, other):
         new_re = self.re - other.re
         new_im = self.im - other.im
-        return f"{new_re} + {new_im}i"
+        return f"{new_re}+{new_im}i"
 
     def __mul__(self, other):
         new_re = (self.re * other.re) - (self.im * other.im)
         new_im = (self.re * other.im) + (self.im * other.re)
-        return f"{new_re} + {new_im}i"
+        return f"{new_re}+{new_im}i"
 
     def __eq__(self, other):
         return ((self.re == other.re) and (self.im == other.im))
